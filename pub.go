ef7960d3d7f523f663e1b04e57573e41e6421ba2
makeus <hhj007v@makeus.com>
2015-09-01 19:38:01 +0900

    main 정

--- pub.go
+++ pub.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	//"./src/database/Splunk"
+	"./src/handlers/activity_handler"
+
+	"./src"
+
+	"./src/middleware/commonlog"
+	"./src/middleware/cors"
 	"./src/middleware/debug"
-	// "./src/handlers"
+	"./src/middleware/recovery"
 
 	"log"
 	"net/http"
 	"runtime"
 	"time"
 
-	"github.com/gin-gonic/gin"
-	// "github.com/itsjamie/gin-cors"
 	"github.com/DeanThompson/ginpprof"
-	"github.com/wblakecaldwell/profiler"
+	"github.com/HyeJong/profiler"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
@@ -27,32 +32,17 @@ func main() {
 	gin.SetMode(gin.ReleaseMode)
 
 	//Webframework gin-tonic Init
-	router := gin.Default()
+	router := gin.New()
 
+	router.Use(commonlog.Logger())
+	router.Use(recovery.Recovery())
+	router.Use(cors.Middleware(config.CORS_CONFIG))
 	//router.Use(gzip.Gzip(gzip.DefaultCompression))	// gzip
 
-	// router.Use(cors.Middleware(cors.Config{		// cors
-	// 	Origins:         "*",
-	// 	Methods:         "GET, PUT, POST, DELETE",
-	// 	RequestHeaders:  "Origin, Authorization, Content-Type",
-	// 	ExposedHeaders:  "",
-	// 	MaxAge:          50 * time.Second,
-	// 	Credentials:     true,
-	// 	ValidateHeaders: false,
-	// }))
-
-	// v1 := router.Group("/singapore")
-	// {
-	// 	v1.POST("/screen", appC.Request)
-	// }
-
-	router.GET("/ping", func(c *gin.Context) {
-		var arr [100]int64
-		if arr[0] != 0 {
-
-		}
-		c.JSON(http.StatusOK, gin.H{"Status": "ok"})
-	})
+	v1 := router.Group("/singapore")
+	{
+		v1.POST("/screen", activity_handler.Request)
+	}
 
 	// profiler for gin
 	profiler.AddMemoryProfilingHandlers(router)
@@ -62,8 +52,8 @@ func main() {
 	// e.g. /debug/pprof, /debug/pprof/heap, etc.
 	ginpprof.Wrapper(router)
 
-	//Listen and server on 0.0.0.0:9090
-	err := func() error {
+	// Listen and server on 0.0.0.0:9090
+	ser := func() error {
 		s := &http.Server{
 			Addr:           ":9090",
 			Handler:        router,
@@ -73,22 +63,8 @@ func main() {
 		}
 		return s.ListenAndServe()
 	}
-	if err != nil {
+
+	if err := ser(); err != nil {
 		log.Println(err)
 	}
-}
-
-func recoverHandler(next http.Handler) http.Handler {
-	fn := func(w http.ResponseWriter, router *http.Request) {
-		defer func() {
-			if err := recover(); err != nil {
-				log.Printf("panic: %+v", err)
-				http.Error(w, http.StatusText(500), 500)
-			}
-		}()
-
-		next.ServeHTTP(w, router)
-	}
-
-	return http.HandlerFunc(fn)
 }
