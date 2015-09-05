4cb37c67e7c9c831a020233edce40fac041f508d
makeus <hhj007v@makeus.com>
2015-09-05 17:43:28 +0900

    cors config 추

--- src/config.go
+++ src/config.go
@@ -1,7 +1,10 @@
 // config
 package config
 
-import ()
+import (
+	"./middleware/cors"
+	"time"
+)
 
 const (
 	NAMESPACE = "st"
@@ -9,3 +12,13 @@ const (
 	SPLUNK_PROTOCOL = "tcp"
 	SPLUNK_ADDR     = "127.0.0.1:9998"
 )
+
+var CORS_CONFIG = cors.Config{
+	Origins:         "*",
+	Methods:         "GET, PUT, POST, DELETE",
+	RequestHeaders:  "Origin, Authorization, Content-Type",
+	ExposedHeaders:  "",
+	MaxAge:          50 * time.Second,
+	Credentials:     true,
+	ValidateHeaders: false,
+}
