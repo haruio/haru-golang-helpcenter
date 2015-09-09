6cd583639852d036e556a796583850130c6c6e44
Hyejong <hhj007v@gamil.com>
2015-09-09 15:45:17 +0900

    custom erros 추가

---
+++ src/config/error/error.go
@@ -0,0 +1,13 @@
+package utility
+
+import (
+	"errors"
+)
+
+var (
+	ErrShortWrite    = errors.New("framework: short write")
+	ErrShortBuffer   = errors.New("framework: short buffer")
+	EOF              = errors.New("framework: EOF")
+	ErrUnexpectedEOF = errors.New("framework: unexpected EOF")
+	ErrNoProgress    = errors.New("framework: multiple Read calls return no data or error")
+)
