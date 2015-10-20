023345b75aebce8edf60ea6d026d90f278154dac
Hyejong <hhj007v@gamil.com>
2015-10-20 16:18:48 +0900

    리팩토링

--- HelpCenter.go
+++ HelpCenter.go
@@ -5,11 +5,9 @@ import (
 	"./src/encoding"
 	"./src/models"
 	"./src/utility"
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/binding"
-	"github.com/martini-contrib/cors"
-	"github.com/martini-contrib/render"
-	// "log"
+	"github.com/itsjamie/gin-cors"
+	"github.com/gin-gonic/gin"
+	"log"
 	// "net/http"
 	"runtime"
 )
@@ -17,6 +15,37 @@ import (
 func main() {
 	runtime.GOMAXPROCS(runtime.NumCPU())
 
+	// Gin(Web framework) Release Mode
+	gin.SetMode(gin.ReleaseMode)
+
+	//Webframework gin-tonic Init
+	r := gin.Default()
+
+	r.Use(cors.Middleware(cors.Config{
+		Origins:        "*",
+		Methods:        "GET, PUT, POST, DELETE",
+		RequestHeaders: "Origin, Authorization, Content-Type",
+		ExposedHeaders: "",
+	}))
+
+	//Simple group: Upload music
+	v1 := r.Group("/1/notice")
+	{
+		v1.GET("/:pushid/ack/:status", appC.Request)
+		v1.GET("/list", controllers.ReadListNotice)
+		v1.GET("/:id", controllers.ReadIdNotice)
+		v1.POST("/add", controllers.CreateNotice)
+		v1.PUT("/:id",controllers.UpdateNotice)
+		v1.DELETE("/:id", controllers.DeleteNotice)
+	}
+
+	//Listen and server on 0.0.0.0:9000
+	err := r.Run(":10600")
+	if err != nil {
+		log.Println(err)
+	}
+
+	/*
 	martini.Env = martini.Prod
 	m := martini.Classic()
 	m.Use(render.Renderer())
@@ -24,16 +53,18 @@ func main() {
 	m.Use(models.InitDB())
 	m.Use(models.InitRabbitMQ())
 	//log.Fatal(http.ListenAndServe(":10600", m))
+
 	f := utility.InitLogger(m)
 	defer f.Close()
+	*/
 
-	m.Use(cors.Allow(&cors.Options{
-		AllowOrigins:     []string{"*"},
-		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE"},
-		AllowHeaders:     []string{"Application-Id", "Content-Type"},
-		ExposeHeaders:    []string{"Content-Length"},
-		AllowCredentials: true,
-	}))
+//	m.Use(cors.Allow(&cors.Options{
+//		AllowOrigins:     []string{"*"},
+//		AllowMethods:     []string{"PUT", "GET", "POST", "DELETE"},
+//		AllowHeaders:     []string{"Application-Id", "Content-Type"},
+//		ExposeHeaders:    []string{"Content-Length"},
+//		AllowCredentials: true,
+//	}))
 
 	m.Group("/1/notice", func(r martini.Router) {
 		r.Get("/list", controllers.ReadListNotice)
--- src/config.go
+++ src/config.go
@@ -3,4 +3,10 @@ package config
 
 import ()
 
-const ()
+const (
+	NAMESPACE = "st"
+
+	RABBITMQ_ADDR      = "amqp://admin:admin@52.68.253.219:5672/"
+	RABBITMQ_QUEUENAME = "feedback"
+
+)
